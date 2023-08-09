cdf3766db9a4d1caed157e32264cb3f3ca018373
santosh <vemulasantosh.k@gmail.com>
2023-08-09 06:40:07 +0000

    solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
+import {Switch, Redirect, Route} from 'react-router-dom'
+
+import Login from './components/Login'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+import Home from './components/Home'
 import './App.css'
 
-// Replace your code here
-const App = () => <h1>Hello World!</h1>
+const App = () => (
+  <Switch>
+    <Route exact path="/ebank/login" component={Login} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <Route exact path="/not-found" component={NotFound} />
+    <Redirect to="not-found" />
+  </Switch>
+)
+
+export default App
